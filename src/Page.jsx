c209684e08f622a07f4f92cccb30d26fdb629fbf
Jesse Akorah <jesse@Jesses-MacBook-Pro.local>
2022-08-06 23:46:18 +0100

    Add more sections

--- src/Page.jsx
+++ src/Page.jsx
@@ -26,7 +26,7 @@ function Page() {
         <section className="about-section section left">
           <div className="section-header">
             <div className="header-top">
-              <span className="section-title-text">About</span>
+              <span className="section-title-text">About me</span>
               <div className="section-title-decoration bar-one"></div>
               <div className="section-title-decoration bar-two"></div>
               <div className="section-title-decoration bar-three"></div>
@@ -50,6 +50,116 @@ function Page() {
                 cumque accusamus.
               </p>
               <h3 className="text-head">Lorem Ipsum</h3>
+              <p className="text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dignissimos dolor consectetur in a natus quidem quos eius cum
+                alias vero porro, repellat vel at voluptate numquam dolorem ipsa
+                cumque accusamus.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <div className="second-margin section-margin"></div>
+
+        <section className="works-section section right">
+          <div className="section-header">
+            <div className="header-top">
+              <span className="section-title-text">My Works</span>
+              <div className="section-title-decoration bar-one"></div>
+              <div className="section-title-decoration bar-two"></div>
+              <div className="section-title-decoration bar-three"></div>
+            </div>
+            <span className="section-number">02</span>
+          </div>
+          <div className="section-content">
+            <div className="section-text">
+              <h3 className="text-head">Lorem Ipsum</h3>
+              <p className="text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dignissimos dolor consectetur in a natus quidem quos eius cum
+                alias vero porro, repellat vel at voluptate numquam dolorem ipsa
+                cumque accusamus.
+              </p>
+              <h3 className="text-head">Lorem Ipsum</h3>
+              <p className="text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dignissimos dolor consectetur in a natus quidem quos eius cum
+                alias vero porro, repellat vel at voluptate numquam dolorem ipsa
+                cumque accusamus.
+              </p>
+              <h3 className="text-head">Lorem Ipsum</h3>
+              <p className="text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dignissimos dolor consectetur in a natus quidem quos eius cum
+                alias vero porro, repellat vel at voluptate numquam dolorem ipsa
+                cumque accusamus.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <div className="second-margin section-margin"></div>
+
+        <section className="works-section section left">
+          <div className="section-content">
+            <div className="section-text">
+              <h3 className="text-head">Lorem Ipsum</h3>
+              <p className="text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dignissimos dolor consectetur in a natus quidem quos eius cum
+                alias vero porro, repellat vel at voluptate numquam dolorem ipsa
+                cumque accusamus.
+              </p>
+              <p className="text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dignissimos dolor consectetur in a natus quidem quos eius cum
+                alias vero porro, repellat vel at voluptate numquam dolorem ipsa
+                cumque accusamus.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <div className="second-margin section-margin"></div>
+
+        <section className="contact-section section left">
+          <div className="section-header">
+            <div className="header-top">
+              <span className="section-title-text">Contact</span>
+              <div className="section-title-decoration bar-one"></div>
+              <div className="section-title-decoration bar-two"></div>
+              <div className="section-title-decoration bar-three"></div>
+            </div>
+            <span className="section-number">03</span>
+          </div>
+          <div className="section-content">
+            <div className="section-text">
+              <h3 className="text-head">Lorem Ipsum</h3>
+              <p className="text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dignissimos dolor consectetur in a natus quidem quos eius cum
+                alias vero porro, repellat vel at voluptate numquam dolorem ipsa
+                cumque accusamus.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <div className="second-margin section-margin"></div>
+
+        <section className="finally-section section right">
+          <div className="section-header">
+            <div className="header-top">
+              <span className="section-title-text">Finally</span>
+              <div className="section-title-decoration bar-one"></div>
+              <div className="section-title-decoration bar-two"></div>
+              <div className="section-title-decoration bar-three"></div>
+            </div>
+            <span className="section-number">04</span>
+          </div>
+          <div className="section-content">
+            <div className="section-text">
               <p className="text">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 Dignissimos dolor consectetur in a natus quidem quos eius cum
